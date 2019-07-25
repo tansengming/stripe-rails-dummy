75db403c8fd9f9229c6920d69ac6ac28c01d6cb3
SengMing Tan <tansengming@gmail.com>
2019-07-25 16:25:38 -0400

    do not pre-load user on test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,6 @@ require 'stripe/rails/testing'
 class UserTest < ActionMailer::TestCase
   test 'test reloading User model' do
     StripeMock.start
-    user = User.new
     Stripe::Rails::Testing.send_event 'customer.updated'
   end
 end
