5f1465e5ec8b63e5afb02f695a3f5c7eb76203ed
SengMing Tan <tansengming@gmail.com>
2019-07-25 16:25:27 -0400

    cleans up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ApplicationRecord
   include Stripe::Callbacks
 
-  def say_tootsie
-    puts 'tootsie!'
-  end
-
   def say_hello
     puts 'hello!'
   end
 
   after_customer_updated! do |_customer, _event|
-    # User.new.say_hello
-    User.new.say_tootsie
+    User.new.say_hello
   end
 end
