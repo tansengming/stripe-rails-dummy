b0b6beefc5ea22b7a0cc0867eeb30cb2e36d93f9
SengMing Tan <tansengming@gmail.com>
2019-07-25 16:45:07 -0400

    adds User#say_bye

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ApplicationRecord
     puts 'hello!'
   end
 
+  def say_bye
+    puts 'bye!'
+  end
+
   after_customer_updated! do |_customer, _event|
     User.new.say_hello
+    # User.new.say_bye
   end
 end
